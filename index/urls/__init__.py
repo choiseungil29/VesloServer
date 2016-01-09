497fbcbd8591238a4c6306eaefaf20afb7445e92
clogic <choiseungil29@gmail.com>
2016-01-09 13:15:16 +0900

    infinite test

--- index/urls/__init__.py
+++ index/urls/__init__.py
@@ -12,12 +12,17 @@ from index import db_session
 def isExistUser(userId):
     query = db_session.query(User).filter_by(id=userId)
     try:
+        print 'what'
         query.one()
     except NoResultFound, e:
+        print 'what2'
         return False
+    print 'what3'
     return True
 
 def isLogin(userId):
     if userId in session:
+        print 'what4'
         return True
+    print 'what5'
     return False
