bc90871a8138eec149a41dd7e1608487476ce546
clogic <choiseungil29@gmail.com>
2016-01-15 01:05:52 +0900

    logging 2

--- index/__init__.py
+++ index/__init__.py
@@ -13,6 +13,12 @@ app.logger.info('secret key ' + app.secret_key)
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://clogic:ok154288tmddlf@localhost/Veslo" # db uri
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://itnpxmghkouqge:DrNcaRJ86jmr4IwHi9NK3mzHfP@ec2-54-83-204-159.compute-1.amazonaws.com:5432/d5inl38ocf5nlu" # db server uri
 
+import logging
+from logging import StreamHandler
+file_handler = StreamHandler()
+file_handler.setLevel(logging.WARNING)
+app.logger.addHandler(file_handler)
+
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
